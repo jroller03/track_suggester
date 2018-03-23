fc293a8a4a4fdc0ce8a25c70c07f6f6efc84dc24
Justin Roller <justin@>
2018-03-23 11:51:42 -0700

    this will add scripts.js code for if branching and inputing information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
 $(document).ready(function(){
   $("form#info").submit(function(event){
-    var company = $("input#company").val();
+    var gender = $("select#gender").val();
+    var company = $("select#company").val();
     var system = $("select#system").val();
     var work_type = $("select#work_type").val();
     var city = $("select#city").val();
+    var typing = $("input#typing").val();
+    var passion = $("select#passion").val();
 
-    if (!age){
+    if (!typing){
       alert("Please fill out all the forms!");
       return;
     }
-
-    if (gender === 'male'){
-      if (){
-        $(".suggest").text("");
+    if (company === 'Microsoft'){
+        $(".suggest").text("C#/.Net");
         $("#route").show();
-      }else if () {
-        $(".suggest").text("");
+      }else if (company === 'Steam') {
+        $(".suggest").text("Java and CSS");
         $("#route").show();
-      } else if (){
-        $(".suggest").text("");
+      } else if (company === 'Godaddy.com'){
+        $(".suggest").text("Java and CSS");
+        $("#route").show();
+      } else if (company === 'Groupon'){
+        $(".suggest").text("Ruby/Rails");
         $("#route").show();
       }
-
-    } else {
-        if () {
-          $(".suggest").text("");
-          $("#route").show();
-        } else if () {
-          $(".suggest").text("");
-          $("#route").show();
-        } else if (){
-          $(".suggest").text("");
-          $("#route").show();
-        }
-    }
     event.preventDefault();
   });
 });
